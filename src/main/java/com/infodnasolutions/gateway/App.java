d4db67e05966fb86d6a310fa0f03cb4afe69abcd
sreejith.variyath@infodnasolutions.com <va.sreejith@gmail.com>
2017-04-25 15:04:34 +0530

    branch one changed again

--- src/main/java/com/infodnasolutions/gateway/App.java
+++ src/main/java/com/infodnasolutions/gateway/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World! THis is branch 1 Changes" );
         System.out.println( "Branch one changed again" );
+        System.out.println( "Branch one changed again" );
     }
 }
